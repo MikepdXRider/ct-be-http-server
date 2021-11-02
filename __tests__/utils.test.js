1e3375d3fca9f4852b60647c77d0f125570498ec
Michael Rider <mikepdxrider@gmail.com>
2021-11-02 09:17:02 -0700

    adds failing parseBody function returns null test

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,11 +1,13 @@
 // import app for testing.
-// const app = require('../lib/app.js');
+const app = require('../lib/app.js');
 // import simpleDB for instance creation and use.
 // const SimpleDb = require('../SimpleDB.js');
 // import request from supertest
-// const request = require('supertest');
+const request = require('supertest');
 // import rmdir and mkdir promises from fs.
 const { rmdir, mkdir } = require('fs/promises');
+// import parseBody function
+const parseBody = require('../lib/utils/parse-body.js');
 
 // create a new route
 const ROOTDIR = './store';
@@ -29,10 +31,10 @@ describe('tests app behaviors', () => {
   //        - Then move to app.js to create the infrastructure to dynamically communicate with the resource map.
   //        - Then move to resource.js to create the resource map to accomplish this task.
   //        - Continue through the list. Then start working on tests for resource router. 
-  it('parseBody returns null if method is not POST, PUT, or PATCH', () => {
-    
+  it('parseBody returns null if method is not POST, PUT, or PATCH', async () => {
+    const actual = await request(app).get('/');
 
-    // expect(actual).toEqual(null);
+    expect(actual.body).toEqual(null);
   });
   //  - throws if content-type is not application/json
   //  - returns deserialized body from req emitted events (using JSON.parse)
